92943175316df6077ad1ee4e572ec40e5d9f112a
ABHRADIP MANDAL <dulalmandal1019@gmail.com>
2025-07-20 23:08:19 +0530

    Initial commit for Doctalk-AI

---
+++ app.js
@@ -0,0 +1,8 @@
+const cookieParser = require("cookie-parser");
+const express = require ("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+app.use(express.json());
